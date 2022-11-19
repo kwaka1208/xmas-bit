b03ec76419e91fb0a70868a07e2d28eef7edf300
Wakabayashi, Kenichi <kwaka1208@crssrds.jp>
2022-11-19 09:34:41 +0900

    ランダムに点灯してみました。

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
 let strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB_RGB)
-strip.showRainbow(1, 360)
+let range = strip.range(0, 12)
+strip.setBrightness(30)
+let count = 0
+for (let index = 0; index < strip.length(); index++) {
+    strip.setPixelColor(count, neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
+    count += 1
+}
+strip.show()
 basic.forever(function () {
-    strip.shift(1)
+    strip.rotate(1)
+    strip.show()
+    basic.pause(200)
 })
