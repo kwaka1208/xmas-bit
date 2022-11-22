4c7166438c7be7eb9c26a2a63b447b30bbbf2014
Wakabayashi, Kenichi <kwaka1208@crssrds.jp>
2022-11-22 23:40:49 +0900

    変数を整理

--- main.ts
+++ main.ts
@@ -1,10 +1,8 @@
-let strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB_RGB)
-let range = strip.range(0, 12)
+let strip = neopixel.create(DigitalPin.P0, 6, NeoPixelMode.RGB_RGB)
+let range = strip.range(0, strip.length())
 strip.setBrightness(30)
-let count = 0
-for (let index = 0; index < strip.length(); index++) {
-    strip.setPixelColor(count, neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
-    count += 1
+for (let カウンター = 0; カウンター <= strip.length() - 1; カウンター++) {
+    strip.setPixelColor(カウンター, neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
 }
 strip.show()
 basic.forever(function () {
