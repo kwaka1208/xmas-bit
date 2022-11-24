3b61cfa0d776eab694267f31f8949d17431cdf32
Wakabayashi, Kenichi <kwaka1208@crssrds.jp>
2022-11-25 06:21:58 +0900

    Add illumination mode

--- main.ts
+++ main.ts
@@ -1,10 +1,56 @@
-let strip = neopixel.create(DigitalPin.P0, 6, NeoPixelMode.RGB_RGB)
+function pattern2 () {
+    for (let カウンター = 0; カウンター <= strip.length() - 1; カウンター++) {
+        if (カウンター % 2 == 0) {
+            strip.setPixelColor(カウンター, neopixel.rgb(255, 255, 255))
+        } else {
+            strip.setPixelColor(カウンター, neopixel.rgb(0, 0, 0))
+        }
+    }
+    strip.show()
+}
+input.onButtonPressed(Button.A, function () {
+    if (mode == MODE_MAX) {
+        mode = 0
+    } else {
+        mode += 1
+    }
+    basic.showNumber(mode)
+    selectMode(mode)
+})
+input.onButtonPressed(Button.B, function () {
+    if (mode == 0) {
+        mode = MODE_MAX
+    } else {
+        mode += -1
+    }
+    basic.showNumber(mode)
+    selectMode(mode)
+})
+function pattern1 () {
+    for (let カウンター = 0; カウンター <= strip.length() - 1; カウンター++) {
+        strip.setPixelColor(カウンター, neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
+    }
+    strip.show()
+}
+function selectMode (_mode: number) {
+    if (_mode == 1) {
+        pattern1()
+    } else if (_mode == 2) {
+        pattern2()
+    } else {
+        strip.clear()
+        strip.show()
+    }
+}
+let MODE_MAX = 0
+let mode = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB_RGB)
 let range = strip.range(0, strip.length())
 strip.setBrightness(30)
-for (let カウンター = 0; カウンター <= strip.length() - 1; カウンター++) {
-    strip.setPixelColor(カウンター, neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
-}
-strip.show()
+mode = 0
+MODE_MAX = 2
+basic.showNumber(mode)
 basic.forever(function () {
     strip.rotate(1)
     strip.show()
