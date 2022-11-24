6b9bacad806de50d4af89da2b83879e9b1dcedd6
Wakabayashi, Kenichi <kwaka1208@crssrds.jp>
2022-11-25 07:08:45 +0900

    Add pattern3

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+function pattern3 () {
+    strip.clear()
+    for (let カウンター = 0; カウンター <= strip.length() - 1; カウンター++) {
+        strip.setPixelColor(カウンター, neopixel.hsl(0, 100 / strip.length() * カウンター, 100 / strip.length() * カウンター))
+    }
+    strip.show()
+}
 function pattern2 () {
     for (let カウンター = 0; カウンター <= strip.length() - 1; カウンター++) {
         if (カウンター % 2 == 0) {
@@ -17,7 +24,6 @@ input.onButtonPressed(Button.A, function () {
     } else {
         mode += 1
     }
-    basic.showNumber(mode)
     selectMode(mode)
 })
 input.onButtonPressed(Button.B, function () {
@@ -26,7 +32,6 @@ input.onButtonPressed(Button.B, function () {
     } else {
         mode += -1
     }
-    basic.showNumber(mode)
     selectMode(mode)
 })
 function pattern1 () {
@@ -36,10 +41,13 @@ function pattern1 () {
     strip.show()
 }
 function selectMode (_mode: number) {
-    if (_mode == 1) {
+    basic.showNumber(_mode)
+    if (mode == 1) {
         pattern1()
     } else if (_mode == 2) {
         pattern2()
+    } else if (_mode == 3) {
+        pattern3()
     } else {
         strip.clear()
         strip.show()
@@ -52,7 +60,7 @@ strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB_RGB)
 let range = strip.range(0, strip.length())
 strip.setBrightness(30)
 mode = 0
-MODE_MAX = 2
+MODE_MAX = 3
 basic.showNumber(mode)
 basic.forever(function () {
     strip.rotate(1)
